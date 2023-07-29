6e948646464e7c376b90ee853c103832cd42064e
Mustafa S. Moiz <msmoiz@amazon.com>
2023-07-28 19:52:05 -0700

    feat: add readme

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,24 @@
+//! This is a simple library for working with TOML files.
+//!
+//! ```rust
+//! use toml::from_str;
+//!
+//! let toml = from_str(r#"
+//! [groceries]
+//! fruit.apples=3
+//! fruit.bananas=5
+//! pasta.sauce="marinara"
+//! pasta.noodles="spaghetti"
+//! cash=true
+//! "#).unwrap();
+//!
+//! assert_eq!(toml["groceries"]["fruit"]["apples"].as_int(), 3);
+//! assert_eq!(toml["groceries"]["fruit"]["bananas"].as_int(), 5);
+//! assert_eq!(toml["groceries"]["pasta"]["sauce"].as_str(), "marinara");
+//! assert_eq!(toml["groceries"]["pasta"]["noodles"].as_str(), "spaghetti");
+//! assert_eq!(toml["groceries"]["cash"].as_bool(), true);
+//! ```
+
 pub use crate::toml::Value;
 pub use error::Result;
 use parser::Parser;
